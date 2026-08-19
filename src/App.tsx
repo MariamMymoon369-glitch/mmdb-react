a667c3a88ab1e3d47f90c9e41e276cdd85b18cf0
Mariam Yasser Mymoon <mariam.me421@gmail.com>
2026-08-19 11:50:29 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/*
 import './App.css';
 import useFetch from './hooks/useFetch';
 import MovieList from './components/MovieList';
@@ -38,6 +39,86 @@ function App() {
         <Route path="/movies/" element={<MovieList movies={movies} />} />
 
       </Routes>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+
+import { useState } from 'react';
+import './App.css';
+import useFetch from './hooks/useFetch';
+import MovieList from './components/MovieList';
+import MovieDetails from './components/movieDetails';
+import { Routes, Route } from 'react-router';
+
+interface Movie {
+  id: string | number;
+  posterUrl: string;
+  title: string;
+  releaseYear: number;
+}
+
+function App() {
+  const { data, loading, error } = useFetch<Movie[]>(
+    'http://localhost:3000/movies'
+  );
+
+  const movies = data ?? [];
+
+  const [search, setSearch] = useState('');
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (movies.length === 0) {
+    return <div>No movies found.</div>;
+  }
+
+  return (
+    <div>
+      <Routes>
+        <Route
+          path="/movies/"
+          element={
+            <>
+              <h1>Movies</h1>
+
+              <input
+                type="text"
+                placeholder="Search movies..."
+                value={search}
+                onChange={(event) => setSearch(event.target.value)}
+              />
+
+              {filteredMovies.length === 0 ? (
+                <p>No movies match "{search}".</p>
+              ) : (
+                <MovieList movies={filteredMovies} />
+              )}
+            </>
+          }
+        />
+
+        <Route path="/movies/:id" element={<MovieDetails />}/>
+
+        <Route path="/" element={<MovieList movies={movies} />}/>
+
+
+      </Routes>
+
+
     </div>
   );
 }
